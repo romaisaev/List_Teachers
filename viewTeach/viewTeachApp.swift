9786d3efc2f98b279a768e411d9062abf3b1f18d
romaisaev <roma.isaev.001@mail.ru>
2020-09-28 16:47:40 +0300

    Initial Commit

---
+++ viewTeach/viewTeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewTeachApp.swift
+//  viewTeach
+//
+//  Created by Рома Исаев on 28.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct viewTeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
